6787d6eac41d2dde0528e1467536e75e681d1bd0
Alextxnk <55585988+Alextxnk@users.noreply.github.com>
2022-03-28 12:01:43 +0300

    Add files via upload

---
+++ data.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+   'about' => [
+      'name' => 'Алексей Соловьев',
+      'post' => 'Back-end Developer',
+      'email' => 'alexsolov766@gmail.com',
+      'phone' => '8(918)298-59-47',
+      'github' => 'https://github.com/Alextxnk',
+      'git' => 'github.com/Alextxnk'
+   ],
+   'education' => [
+      [
+         'faculty' => 'Computer Science and Engineering',
+         'university' => 'Don State Technical University',
+         'yearStartEdu' => '2019',
+         'yearEndEdu' => '2023'
+      ]
+      ],
+   'languageNative' => [
+      'English'
+   ],
+   'languageProfessional' => [
+      'Russian'
+   ],
+   'interests' => [
+      'Reading' , 'Gymming', 'Cooking'
+   ],
+   'aboutCareer' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam esse, corrupti molestias illo modi expedita autem rem vitae odit magni consectetur repellat adipisci velit minus architecto placeat, quo, dignissimos quam.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam esse, corrupti molestias illo modi expedita autem rem vitae odit magni consectetur repellat adipisci velit minus architecto placeat, quo, dignissimos quam.',
+   'experiences' => [
+      [
+         'post' => 'Back-end Developer',
+         'placeOfWork' => 'Accenture, Rostov-on-Don',
+         'duty' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam esse, corrupti molestias illo modi expedita autem rem vitae odit magni consectetur repellat adipisci velit minus architecto placeat, quo, dignissimos quam.', // обязанности
+         'yearStartWork' => '2021',
+         'yearEndWork' => 'Present'
+      ]
+   ]
+];
